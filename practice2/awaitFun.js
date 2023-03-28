5de461996986bf6125778f83569733f59cefcf2c
mehmooda kiran <mehmoodakiran9@gmail.com>
2023-03-27 19:41:31 -0700

    Await file

---
+++ practice2/awaitFun.js
@@ -0,0 +1,11 @@
+async function test() {
+    console.log("2:message");
+    await console.log("3:message");
+    // const response = await fetch(""); 
+    // const students=await Response.json();
+    console.log("4:message");
+}
+console.log("1:message");
+test()
+console.log("5:message");
+console.log("6:message");
